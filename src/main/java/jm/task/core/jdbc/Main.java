57ae4a71dfa4cf353aedd2beb214f201abe9970b
jinjony <104268946+jinjony@users.noreply.github.com>
2022-06-16 03:19:11 +0300

    ~

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.util.Util;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util.getConnection();
+        UserDao userDao = new UserDaoJDBCImpl();
+
+        userDao.createUsersTable();
+
+        userDao.saveUser("Morgan", "Freeman", (byte) 85);
+        userDao.saveUser("Clint", "Eastwood", (byte) 92);
+        userDao.saveUser("Johny", "Depp", (byte) 59);
+        userDao.saveUser("Angelina", "Jolie", (byte) 47);
+
+        userDao.removeUserById(1);
+        userDao.getAllUsers();
+        userDao.cleanUsersTable();
+        userDao.dropUsersTable();
     }
+
 }
